780e72cce818c4837ea2fac79267fcf0cf588780
Cristina Simoncini <crisimonc@gmail.com>
2018-11-09 10:59:14 +0100

    TDD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,17 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.create(product_params)
+    redirect_to root_path
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
